62736a735138dc3786db28f1f6295595d631552f
myu <83861825+geeknow112@users.noreply.github.com>
2023-07-04 10:57:42 +0900

    Create engineer-freelance-qiita-sample_3.py

---
+++ code/engineer-freelance-qiita-sample_3.py
@@ -0,0 +1,16 @@
+# Hello, world!
+print('Hello, world!')
+
+# 変数の宣言
+name = 'Alice'
+age = 30
+
+# 条件分岐
+if age >= 20:
+	print(name + ' is an adult.')
+else:
+	print(name + ' is a child.')
+
+# 繰り返し処理
+for i in range(5):
+	print(i)
