f57efad57e4e279f9c81f0416232bc7719ae35f2
przorzcrz <przorzcrz@gmail.com>
2023-07-07 01:21:34 +0000

    python-comparison-operator.py

---
+++ code/python-comparison-operator.py
@@ -0,0 +1,28 @@
+'''```python'''
+# 数値の比較
+a = 10
+b = 5
+print(a > b)  # 出力結果: True
+print(a < b)  # 出力結果: False
+print(a == b)  # 出力結果: False
+print(a != b)  # 出力結果: True
+
+# 文字列の比較
+str1 = "apple"
+str2 = "banana"
+print(str1 > str2)  # 出力結果: False
+print(str1 < str2)  # 出力結果: True
+print(str1 == str2)  # 出力結果: False
+print(str1 != str2)  # 出力結果: True
+'''```'''
+
+
+'''```python'''
+# 複数の比較演算子の使用
+a = 10
+b = 5
+c = 3
+print(a > b and a > c)  # 出力結果: True
+print(a > b or a < c)  # 出力結果: True
+'''```'''
+
