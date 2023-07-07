c6b850aea3773012e75d2029af116d44c66a02a4
myu <83861825+geeknow112@users.noreply.github.com>
2023-07-07 10:38:16 +0900

    Create python-for-statement.py

---
+++ code/python-for-statement.py
@@ -0,0 +1,26 @@
+# ```python
+for 変数 in イテラブル:
+	処理
+# ```
+
+
+# ```python
+numbers = [1, 2, 3, 4, 5]
+for n in numbers:
+	print(n * 2)
+# ```
+
+
+# ```python
+for i in range(3):
+	for n in range(5):
+		print(n * 2)
+	print("====")
+# ```
+
+
+# ```python
+string = "hello"
+for s in string:
+	print(s)
+# ```
