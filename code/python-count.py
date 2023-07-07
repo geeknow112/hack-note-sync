c5808eccc9eabbdc379218175134d86f671a6745
przorzcrz <przorzcrz@gmail.com>
2023-07-07 01:24:14 +0000

    python-count.py

---
+++ code/python-count.py
@@ -0,0 +1,21 @@
+'''```python'''
+numbers = [1, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5]
+'''```'''
+
+
+'''```python'''
+count_3 = numbers.count(3)
+print(count_3)
+'''```'''
+
+
+'''```python'''
+text = "Python is a popular programming language."
+'''```'''
+
+
+'''```python'''
+count_a = text.count("a")
+print(count_a)
+'''```'''
+
