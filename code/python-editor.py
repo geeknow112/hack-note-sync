cc9a05da46602ffd9a204de90971f3fd6c9dbcba
przorzcrz <przorzcrz@gmail.com>
2023-07-07 01:28:40 +0000

    python-editor.py

---
+++ code/python-editor.py
@@ -0,0 +1,15 @@
+# ```python
+# Hello, PyCharm!
+print("Hello, PyCharm!")
+# ```
+
+# ```python
+# Hello, VS Code!
+print("Hello, VS Code!")
+# ```
+
+
+# ```python
+# Hello, Jupyter Notebook!
+print("Hello, Jupyter Notebook!")
+# ```
