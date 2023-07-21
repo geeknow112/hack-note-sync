79f50f7223735101ee54e649c4d0309e4bdf71b7
myu <83861825+geeknow112@users.noreply.github.com>
2023-07-21 18:14:50 +0900

    Create docker-install.py

---
+++ code/docker-install.py
@@ -0,0 +1,18 @@
+# ```bash
+docker build -t イメージ名 Dockerfileがあるディレクトリのパス
+# ```
+
+
+# ```bash
+docker run -d -p ホストポート:コンテナポート イメージ名
+# ```
+
+
+# ```bash
+docker stop コンテナIDまたはコンテナ名
+# ```
+
+
+# ```bash
+docker rm コンテナIDまたはコンテナ名
+# ```
