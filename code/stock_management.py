eecbc9a42bef3af1f9be1525320a07c9e6b6f1b1
myu <83861825+geeknow112@users.noreply.github.com>
2023-08-03 14:56:30 +0900

    Update stock_management.py

--- code/stock_management.py
+++ code/stock_management.py
@@ -24,23 +24,26 @@ class StockManagement:
 
 	def add_sub_menu(self):
 		cur_user = wp_get_current_user()
-
+	
 		if cur_user.roles[0] == 'administrator':
 			if cur_user.user_login in ['admin']:
 				# 登録画面
 				add_submenu_page('stock-management', '商品登録', '🔷商品登録', 'read', 'goods-detail', self.goods_detail)
 				add_submenu_page('stock-management', '顧客登録', '🔷顧客登録', 'read', 'customer-detail', self.customer_detail)
 				add_submenu_page('stock-management', '注文登録', '🔷注文登録', 'read', 'sales-detail', self.sales_detail)
-
+	
 				# 検索画面
 				add_submenu_page('stock-management', '商品検索', '🔶商品検索', 'read', 'goods-list', self.goods_list)
 				add_submenu_page('stock-management', '顧客検索', '🔶顧客検索', 'read', 'customer-list', self.customer_list)
 				add_submenu_page('stock-management', '注文検索', '🔶注文検索', 'read', 'sales-list', self.sales_list)
-
+	
 				# その他
 				add_submenu_page('stock-management', 'ロット番号登録', 'ロット番号登録', 'read', 'lot-regist', self.lot_regist)
 				add_submenu_page('stock-management', '配送予定表③', '配送予定表③', 'read', 'delivery-graph', self.delivery_graph)
 				add_submenu_page('stock-management', '日別商品集計', '日別商品集計', 'read', 'sum-day-goods', self.sum_day_goods)
+			else:
+				self.remove_menus()
 		else:
 			self.remove_menus()
-
+			add_action('admin_bar_menu', 'remove_admin_bar_menus', 999)
+		
