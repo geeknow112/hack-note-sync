64ca66d38dc765f9d94fcb80062e5e91769a9709
myu <83861825+geeknow112@users.noreply.github.com>
2023-08-03 14:53:40 +0900

    Create stock_management.py

---
+++ code/stock_management.py
@@ -0,0 +1,46 @@
+import os
+from stock_management.model.model import *
+from stock_management.model.Shop import *
+from stock_management.model.Applicant import *
+from stock_management.model.Sales import *
+from stock_management.model.Goods import *
+from stock_management.model.Customer import *
+from phpoffice.phpspreadsheet import Spreadsheet
+from phpoffice.phpspreadsheet.writer import Xlsx
+
+import pytz
+from datetime import datetime
+
+class StockManagement:
+
+	def __init__(self):
+		self.add_action('admin_menu', self.add_pages)
+		self.add_action('admin_menu', self.add_sub_menu)
+		# self.add_action('init', self.export_csv)
+		# self.add_action('init', self.export_pdf)
+
+	def add_pages(self):
+		add_menu_page('在庫/発注予定管理', '在庫/発注予定管理', 'level_8', 'stock-management', self.menu_top, '', 26)
+
+	def add_sub_menu(self):
+		cur_user = wp_get_current_user()
+
+		if cur_user.roles[0] == 'administrator':
+			if cur_user.user_login in ['admin']:
+				# 登録画面
+				add_submenu_page('stock-management', '商品登録', '🔷商品登録', 'read', 'goods-detail', self.goods_detail)
+				add_submenu_page('stock-management', '顧客登録', '🔷顧客登録', 'read', 'customer-detail', self.customer_detail)
+				add_submenu_page('stock-management', '注文登録', '🔷注文登録', 'read', 'sales-detail', self.sales_detail)
+
+				# 検索画面
+				add_submenu_page('stock-management', '商品検索', '🔶商品検索', 'read', 'goods-list', self.goods_list)
+				add_submenu_page('stock-management', '顧客検索', '🔶顧客検索', 'read', 'customer-list', self.customer_list)
+				add_submenu_page('stock-management', '注文検索', '🔶注文検索', 'read', 'sales-list', self.sales_list)
+
+				# その他
+				add_submenu_page('stock-management', 'ロット番号登録', 'ロット番号登録', 'read', 'lot-regist', self.lot_regist)
+				add_submenu_page('stock-management', '配送予定表③', '配送予定表③', 'read', 'delivery-graph', self.delivery_graph)
+				add_submenu_page('stock-management', '日別商品集計', '日別商品集計', 'read', 'sum-day-goods', self.sum_day_goods)
+		else:
+			self.remove_menus()
+
